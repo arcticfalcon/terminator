83dfc023a91b2fd31acbb0830ace60a9a94cbd0d
Juan Falcón <jcfalcon@gmail.com>
2019-11-23 13:33:37 -0300

    History

--- src/Terminator.js
+++ src/Terminator.js
@@ -1,8 +1,29 @@
 import {observable} from 'mobx'
 import {COMMAND, RESULT} from "./components/OutputList";
 
+const internalCommands = [
+    {
+        "name": "help",
+        "description": "List available commands",
+        "handler": (params, terminator, abortSignal) => {
+            terminator.addResult("Commands")
+            terminator.commands.forEach(com => terminator.addResult(`  ${com.name}\t\t${com.description}`))
+            terminator.done()
+        }
+    },
+    {
+        "name": "history",
+        "description": "List executed commands",
+        "handler": (params, terminator, abortSignal) => {
+            terminator.history.forEach((h, i) => h ? terminator.addResult(`  ${i}  ${h}`) : false)
+            terminator.done()
+        }
+    },
+]
 
 export default class Terminator {
+    history = [""]
+    historyCurrent = 0
     @observable items = []
     @observable executing = false
     @observable fullScreenMode = false
@@ -12,7 +33,7 @@ export default class Terminator {
     abortSignal
 
     constructor(commands) {
-        this.commands = commands;
+        this.commands = [...internalCommands, ...commands];
     }
 
     addExecution(content) {
@@ -41,15 +62,35 @@ export default class Terminator {
         if (this.executing) {
             return
         }
-
-        const commandParts = commandInput.trim().split(" ")
-        // Grab first word
-        const commandStr = commandParts.shift() || ""
-
-        this.addExecution(commandStr)
-        if (commandStr === "") {
+        commandInput = commandInput.trim()
+        this.addExecution(commandInput)
+        if (commandInput === "") {
             return
         }
+
+        // Event command
+        if(commandInput.startsWith("!")) {
+            const index = Number(commandInput.replace("!", ""))
+            if(isFinite(index)) {
+                if(index <= 0 || index >= this.history.length) {
+                    // Error
+                    this.addExecution(commandInput)
+                    this.addResult(`${commandInput}: event not found`)
+                    this.input = ""
+                    return
+                }
+                // Execute from history
+                commandInput = this.history[index]
+                this.addResult(commandInput)
+            }
+        }
+
+        // Add to history
+        this.history.push(commandInput)
+
+        // Grab first word
+        const commandParts = commandInput.split(" ")
+        const commandStr = commandParts.shift() || ""
         // Find command
         const command = this.commands.filter(com => com.name === commandStr).shift()
         if (!command) {
@@ -81,6 +122,21 @@ export default class Terminator {
         if (commands.length === 1) {
             this.setInput(`${commands.shift().name} `)
         }
+    }
+
+    historyUp() {
+        if (this.historyCurrent === 0) {
+            this.historyCurrent = this.history.length
+        }
+        this.historyCurrent = this.historyCurrent <= 1 ? this.historyCurrent : this.historyCurrent - 1
+        this.setInput(this.history[this.historyCurrent])
+    }
+    historyDown() {
+        if (this.historyCurrent === 0) {
+            return
+        }
+        this.historyCurrent = this.historyCurrent >= this.history.length - 1 ? 0 : this.historyCurrent + 1
+        this.setInput(this.history[this.historyCurrent])
     }
 
     setInput(str) {
